cd76a16026a1823af36d34d5fc0410ea76dd72d4
Brit Butler <redline6561@gmail.com>
2013-10-04 14:14:41 -0400

    The first 90% of the Common Lisp for Clojurists talk.

--- talks/clfj.lisp
+++ talks/clfj.lisp
@@ -28,37 +28,108 @@
   (:article
    (:h3 (:pre "> Talk Overview"))
    (:br)
-   (:ol
-    (:li "Common Lisp - the Language") (:br)
-    (:li "Common Lisp - the Toolset") (:br)
-    (:li "Common Lisp - the Ecosystem") (:br)
-    (:li "A Few Projects (time allowing)")))
+   (:p "There " (:em (:b "is")) " a plan but I can switch styles if you want:")
+   (:ul
+    (:li "Give, ya know, a lecture-ish thing and answer questions. :)") (:br)
+    (:li "Walk you through code from script to small lib to bigger.") (:br)
+    (:li "Just live code something audience suggested and narrate."))
+   (:br) (:br)
+   (:p "Let's start with the lecture and veer off course when ready."))
+
+  (:article
+   (:h3 (:pre "> Disclaimers"))
+   (:br)
+   (:p "You'll probably miss some things...")
+   (:ul
+    (:li "Pervasive Destructuring")
+    (:li "Uniform Data Structure APIs")
+    (:li "Lisp-1 Namespacing"))
+   (:br)
+   (:p "And so on. But Clojure and CL are complimentary tools."))
+
+  (:article
+   (:h3 (:pre "> But Why Common Lisp?"))
+   (:br)
+   (:p "Should probably just address this head on...")
+   (:ul
+    (:li "Maybe the JVM isn't an option.")
+    (:li "Maybe you're single-threaded or want mutation.")
+    (:li "Maybe you want to dig under the abstractions.")
+    (:li "Maybe you want to play with the wealth of CL libraries."))
+   (:br)
+   (:p "We can discuss this in much greater depth over beers. :)"))
+
+  (:article
+   (:h3 (:pre "> Some Historical Perspective"))
+   (:br)
+   (:p "Common Lisp is to Clojure as OCaml is to Scala.") (:br)
+   (:p "CL + OCaml reached \"adulthood\" in the mid-90s.") (:br)
+   (:p "Both facing off against C++ as opposed to Java..." (:br)
+       "And thus more suited to \"Systems Programming\".") (:br)
+   (:p "The Ecosystems can seem dated ... like CPAN."))
+
+  (:article
+   (:h3 (:pre "> The Toolset"))
+   (:br)
+   ;; Remember to mention the analogs here: clj, nil, maven, lein
+   (:p "You need: Compiler, Editor, Build Tool, Package Manager")
+   (:br)
+   (:p "Compiler: Just Use " (:a :href "http://sbcl.org/" "SBCL") " or "
+       (:a :href "http://ccl.clozure.com/" "CCL") "." (:br)
+       (:small (:i "(There is ABCL if you must JVM, ECL if you must embedded.)")))
+   (:p "Editor: Premier editors are Emacs/"
+       (:a :href "http://common-lisp.net/project/slime/" "slime") " and vim/"
+       (:a :href "https://bitbucket.org/kovisoft/slimv/overview" "slimv") ".")
+   (:p "Build Tool: It's simple, there is only "
+       (:a :href "http://common-lisp.net/project/asdf/" "ASDF") ".")
+   (:p "Package Manager: Get " (:a :href "http://quicklisp.org/" "quicklisp")
+       ". There is no substitute. :)")
+   (:br)
+   (:p (:a :href "http://common-lisp.net/project/lispbox/" "Lispbox") " is also an option."))
+
+  (:article
+   (:h3 (:pre "> The \"Community\""))
+   (:br)
+   (:p "A few notable, minimally intersecting hangouts...")
+   (:ul
+    (:li (:a :href "http://www.cliki.net/IRC" "#lisp on Freenode")) (:br)
+    (:li (:a :href "http://reddit.com/r/lisp/" "Lisp Reddit")) (:br)
+    (:li (:a :href "http://www.lispforum.com/" "Lisp Forum")) (:br)
+    (:li (:a :href "http://planet.lisp.org/" "Planet Lisp"))))
+
+  (:article
+   (:h3 (:pre "> The Ecosystem"))
+   (:br)
+   (:p "Or, \"What lib do I use for X?\"")
+   (:ul
+    (:li "Check " (:a :href "http://www.cliki.net/current%20recommended%20libraries"
+                      "Cliki's recommended libs"))
+    (:li "Ask in #lisp or use " (:code "(ql:system-apropos \"my-lib\")")))
+   (:br)
+   (:p "What about learning materials?")
+   (:ul
+    (:li "Start with " (:a :href "http://gigamonkeys.com/book/" "Practical Common Lisp") "!")
+    (:li "Take language questions to " (:a :href "http://minispec.org/" "the Minispec") ", "
+         (:br) "take library questions to " (:a :href "http://quickdocs.org/" "QuickDocs") ".")))
+
 
   (:article
    (:h3 (:pre "> The Language"))
    (:br))
 
   (:article
-   (:h3 (:pre "> The Toolset"))
-   (:br))
-
-  (:article
-   (:h3 (:pre "> The Ecosystem"))
-   (:br))
-
-  (:article
-   (:h3 (:pre "> A Few Projects: "
+   (:h3 (:pre "> Selected Projects: "
               (:a :href "https://github.com/redline6561/cl-6502" "famiclom")))
    (:p (:img :src "trp/famiclom.png"))
    (:p :align :center "A (wip) Nintendo Emulator in Pure CL")
    (:ul
     (:li "750 lines of code for the CPU, Assembly, Disassembly.")
-    (:li "Runs at ~50 Mhz on an old Core 2 Duo.")
+    (:li "CPU runs at ~50 Mhz on an old Core 2 Duo.")
     (:li "There's a literate-style "
          (:a :href "http://redlinernotes.com/docs/cl-6502.pdf" "book")"!")))
 
   (:article
-   (:h3 (:pre "> A Few Projects: "
+   (:h3 (:pre "> Selected Projects: "
               (:a :href "https://github.com/redline6561/coleslaw" "coleslaw")))
    (:p :align :center (:img :src "clfj/coleslaw.jpg"))
    (:p :align :center "A Static Blog Engine a la Jekyll")
@@ -71,7 +142,7 @@
          "LaTeX via Mathjax, Wordpress Importing, etc")))
 
   (:article
-   (:h3 (:pre "> A Few Projects: "
+   (:h3 (:pre "> Selected Projects: "
               (:a :href "https://github.com/redline6561/cl-scrobbler" "cl-scrobbler")))
    (:p (:img :src "clfj/shuffletron.png")))
 
@@ -97,15 +168,8 @@
 -- repl tools: trace, break, apropos, describe, time, etc etc etc
 -- libraries for crazy stuff: screamer, optima, lfarm/lparallel, xecto?
 
-2. Implementation and Tools (compilers, editor integration, build system, library management)
--- just sbcl or abcl, can talk more about this over beers/stackoverflow/twitter if you want
--- emacs/SLIME is the premier editor, surprise surprise. vim/slimv is there too.
--- ASDF is the buildsystem, mostly just interact with it via "ASD files" (think project.clj)
--- quicklisp is the library manager (think lein deps), not command line though, in-image!
-
-3. Community and Ecosystem (docs, specific libraries, irc/reddit)
--- CLHS, sure. but also minispec.org, quickdocs
--- cliki current recommended libraries
--- wondering what to use for X? use quicklisp:system-apropos, ask in #lisp.
-
+3. Community and Ecosystem
+-- mention reference cards at clfj/references/, CLHS and other good books at the end...
+   (:li "OO: Kleene, AMOP; macrology: On Lisp, Let over Lambda;" (:br)
+   "general: Land of Lisp, PAIP, Lisp in Small Pieces.")))
 |#
